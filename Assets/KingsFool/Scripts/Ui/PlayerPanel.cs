a91d3b5738de59c3ea571fe24c6cceb9dab7db2d
Colin <colinvaufrey@gmail.com>
2024-01-27 00:38:30 +0100

    Add new assets and scripts

---
+++ Assets/KingsFool/Scripts/Ui/PlayerPanel.cs
@@ -0,0 +1,10 @@
+using TMPro;
+using UnityEngine;
+
+public class PlayerPanel : MonoBehaviour
+{
+    private int playerNumber;
+
+    [SerializeField] TMP_Text playerNumberText;
+    [SerializeField] TMP_Text controllerText;
+}
