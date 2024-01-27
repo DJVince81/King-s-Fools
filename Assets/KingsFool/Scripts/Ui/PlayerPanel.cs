b16c6f639a25d0cec63c8684a71f28b07e087b00
Colin <colinvaufrey@gmail.com>
2024-01-27 02:45:55 +0100

    Update PlayerPanel UI elements

--- Assets/KingsFool/Scripts/Ui/PlayerPanel.cs
+++ Assets/KingsFool/Scripts/Ui/PlayerPanel.cs
@@ -6,7 +6,9 @@ public class PlayerPanel : MonoBehaviour
     [SerializeField] TMP_Text playerNumberText;
     [SerializeField] TMP_Text controllerText;
 
-    public void Initialize()
+    public void Initialize(int playerNumber, string controllerName)
     {
+        playerNumberText.text = $"Player {playerNumber}";
+        controllerText.text = controllerName;
     }
 }
