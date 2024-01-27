d04996b805e9492e1fa7f1c6dfa746a818bcccfa
DJVince81 <39565195+DJVince81@users.noreply.github.com>
2024-01-27 06:47:16 +0100

    Add Launch New Game & Randomizer King/Fool

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
+    [SerializeField] private GameObject FoolPlayer;
+    [SerializeField] private GameObject KingPlayer;
+
     private bool oneTime = true;
+
+    private static Dictionary<int, PlayerInput> players;
 
     private void Awake()
     {
@@ -20,7 +29,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        players = new Dictionary<int, PlayerInput>();
     }
 
     // Update is called once per frame
@@ -34,11 +43,45 @@ public class GameManager : MonoBehaviour
         if (oneTime)
         {
             oneTime = false;
+            players.Clear();
             for (int i = 0; i < MainPanel.Instance.nbPlayer; i++)
             {
-                Debug.Log(MainPanel.Instance.transform.GetChild(i).GetComponent<PlayerInput>());
+                players.Add(i, MainPanel.Instance.transform.GetChild(i).GetComponent<PlayerInput>());
+            }
+            if (MainPanel.Instance.nbPlayer >= 3)
+            {
+                SceneManager.sceneLoaded += OnSceneLoaded;
+                SceneManager.LoadScene("Main");
             }
             Invoke("ResetOneTime", 0.25f);
+        }
+    }
+
+    private static void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Vérifier si la scène chargée est celle que nous attendons
+        if (scene.name == "Main")
+        {
+            // Création des nouveaux PlayerInput dans la nouvelle scène
+            bool kingSpawned = false;
+            for (int i = 0; i < players.Count; i++)
+            {
+                bool isKing = (UnityEngine.Random.Range(0, 10) > 7) && !kingSpawned;
+                GameObject nouveauJoueur = Instantiate(isKing?Instance.KingPlayer:Instance.FoolPlayer);
+                nouveauJoueur.name = (isKing?"King":"Fool") + (i + 1);
+                SceneManager.MoveGameObjectToScene(nouveauJoueur, scene);
+            }
+
+            // Se désabonner de l'événement après l'avoir géré
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+    }
+
+    private void LoadPlayers()
+    {
+        for (int i = 0; i < players.Count; i++)
+        {
+            Debug.Log(players[i].gameObject.name);
         }
     }
 
