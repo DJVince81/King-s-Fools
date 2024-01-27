2790ada728681b372a6fa764d438e91d5a1ce2ff
DJVince81 <39565195+DJVince81@users.noreply.github.com>
2024-01-27 03:45:51 +0100

    Player Disconnect

--- Assets/AutoMove.cs
+++ Assets/AutoMove.cs
@@ -1,21 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AutoMove : MonoBehaviour
 {
+    private PlayerInput playerInput;
 
     // Start is called before the first frame update
     void Start()
     {
         gameObject.transform.SetParent(MainPanel.Instance.transform);
         gameObject.transform.localScale = Vector3.one;
-        Debug.Log("Move gameObject");
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+    }
+
+    public void Disconnect()
+    {
+        Debug.Log("Disconnect");
+        Destroy(gameObject);
     }
 }
